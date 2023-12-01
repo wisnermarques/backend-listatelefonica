e0ba1ce433ec51578b295524566c84570984ab43
wisner <wisner.a.marques@gmail.com>
2023-11-30 21:50:21 -0300

    corrigido

--- index.js
+++ index.js
@@ -26,6 +26,4 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
